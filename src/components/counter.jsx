eacce0f089445121e582392a6bb44c97ffe0cc2b
Mokshith Pyla <mokshithpyla@gmail.com>
2020-10-07 21:25:37 +0530

    basic

---
+++ src/components/counter.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}> {this.formatCount()} </span>
+        <button onClick={() => this.props.onIncrement(this.props.counter)} className="btn btn-secondary btn-sm"> Increment </button>
+        <button onClick={() => this.props.onDecrement(this.props.counter)} className="btn btn-secondary btn-sm m-2"> Decrement </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)} 
+          className="btn btn-danger btn-sm m-2">
+          Delete
+        </button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.props.counter.value === 0 ? 'warning' : 'primary';
+    return classes;
+  }
+
+  formatCount() {
+    const { value } = this.props.counter;
+    return value === 0 ? 'Zero': value;
+  }
+
+  // handleIncrement = product => {
+  //   console.log(product);
+  //   this.setState({ count: this.state.count + 1});
+  // }
+
+  // handleDecrement = () => {
+  //   this.setState({ count: this.state.count - 1});
+  // }
+}
+
+export default Counter;
